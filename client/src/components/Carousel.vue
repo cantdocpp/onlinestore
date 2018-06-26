6a5b396b4dfc58c4d1af60f9ca58e4abb83eb35f
cantdocpp <cihuyman95@gmail.com>
2018-06-27 00:16:22 +0700

    adding basic navigation menu for the site

---
+++ client/src/components/Carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="">
+    <!-- <i class="material-icons">menu</i> -->
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+
+    <h1>this is carousel</h1>
+    <h1>this is carousel</h1>
+
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
